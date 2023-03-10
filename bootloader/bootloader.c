6da633ebb419b62cd80c5e50258790d8a75aa0c9
Viktor Ciolkowski <wikcioo@gmail.com>
2023-03-10 20:24:56 +0100

    Moved from debug_printf to BL_LOG for logging.

--- bootloader/bootloader.c
+++ bootloader/bootloader.c
@@ -11,12 +11,12 @@ int main()
 
     if (gpio_read_pin(GPIOC, GPIO_PIN_13) == GPIO_PIN_LOW)
     {
-        debug_printf("BOOTLOADER_DEBUG: Executing bootloader interactive mode.\n");
+        BL_LOG("Executing bootloader interactive mode.\n");
         bootloader_start_interactive_mode();
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: Executing user application.\n");
+        BL_LOG("Executing user application.\n");
         bootloader_goto_application();
     }
 
@@ -25,11 +25,11 @@ int main()
 
 void bootloader_goto_application(void)
 {
-    debug_printf("BOOTLOADER_DEBUG: Executing bootloader_goto_application.\n");
+    BL_LOG("Executing bootloader_goto_application.\n");
 
     /* We assume that the application firmware is stored in sector 2 of the flash memory */
     uint32_t msp = *(volatile uint32_t *)FLASH_SECTOR_2_BASE_ADDR;
-    debug_printf("BOOTLOADER_DEBUG: MSP value = %#X\n", msp);
+    BL_LOG("MSP value = 0x%08lX\n", msp);
 
     /* Set main stack pointer */
     __asm volatile("MSR MSP, %0"::"r"(msp));
@@ -37,7 +37,7 @@ void bootloader_goto_application(void)
     uint32_t reset_handler_addr = *(volatile uint32_t *)(FLASH_SECTOR_2_BASE_ADDR + 0x4);
     void (*application_reset_handler)(void) = (void *)reset_handler_addr;
 
-    debug_printf("BOOTLOADER_DEBUG: Application reset handler address = %#X\n", application_reset_handler);
+    BL_LOG("Application reset handler address = 0x%08lX\n", (unsigned long)application_reset_handler);
     application_reset_handler();
 }
 
@@ -80,7 +80,7 @@ void bootloader_start_interactive_mode(void)
             bootloader_cmd_mem_read(rx_buffer);
             break;
         default:
-            debug_printf("BOOTLOADER_DEBUG: Error {Unknown command}\n");
+            BL_LOG("Error {Unknown command}\n");
         }
     }
 }
@@ -93,7 +93,7 @@ uint8_t bootloader_verify_crc(uint8_t *data, uint32_t length, uint32_t host_crc)
         uint32_t value = data[i];
         crc_value = crc_accumulate(CRC, &value, 1);
     }
-    debug_printf("BOOTLOADER_DEBUG: CRC value = %#X\n", crc_value);
+    BL_LOG("CRC value = 0x%08lX\n", crc_value);
     /* Reset CRC afterwards so that next time it starts accumulating with no previous value. */
     CRC->CR |= 1 << CRC_CR_RESET;
 
@@ -123,18 +123,18 @@ void bootloader_cmd_get_version(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_get_version.\n");
+    BL_LOG("Called bootloader_cmd_get_version.\n");
     if (!bootloader_verify_crc(buffer, packet_length - 4, host_crc))
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
         bl_version = bootloader_get_version();
-        debug_printf("BOOTLOADER_DEBUG: BL_VERSION = %d (%#X)\n", bl_version, bl_version);
+        BL_LOG("BL_VERSION = %d (%#02X)\n", bl_version, bl_version);
         bootloader_send_ack(1);
         bootloader_send_data(&bl_version, 1);
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -144,17 +144,17 @@ void bootloader_cmd_get_help(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_get_help.\n");
+    BL_LOG("Called bootloader_cmd_get_help.\n");
     if (!bootloader_verify_crc(buffer, packet_length - 4, host_crc))
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
         uint8_t length = sizeof(supported_commands);
         bootloader_send_ack(length);
         bootloader_send_data(supported_commands, length);
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -164,18 +164,18 @@ void bootloader_cmd_get_device_id(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_get_device_id.\n");
+    BL_LOG("Called bootloader_cmd_get_device_id.\n");
     if (!bootloader_verify_crc(buffer, packet_length - 4, host_crc))
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
         uint16_t dev_id = bootloader_get_device_id();
-        debug_printf("BOOTLOADER_DEBUG: DEVICE_ID = %#X\n", dev_id);
+        BL_LOG("DEVICE_ID = %#04X\n", dev_id);
         bootloader_send_ack(2);
         bootloader_send_data((uint8_t *)&dev_id, 2);
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -185,18 +185,18 @@ void bootloader_cmd_get_rdp_level(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_get_rdp_level.\n");
+    BL_LOG("Called bootloader_cmd_get_rdp_level.\n");
     if (!bootloader_verify_crc(buffer, packet_length - 4, host_crc))
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
         uint8_t rdp_level = bootloader_get_rdp_level();
-        debug_printf("BOOTLOADER_DEBUG: RDP LEVEL = %#X\n", rdp_level);
+        BL_LOG("RDP LEVEL = %#02X\n", rdp_level);
         bootloader_send_ack(1);
         bootloader_send_data(&rdp_level, 1);
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -206,21 +206,21 @@ void bootloader_cmd_jump_address(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_jump_address.\n");
+    BL_LOG("Called bootloader_cmd_jump_address.\n");
 
     if (!bootloader_verify_crc(buffer, packet_length - 4, host_crc))
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
         bootloader_send_ack(1);
 
         uint32_t jump_addr = *(uint32_t *)(buffer + 2);
-        debug_printf("BOOTLOADER_DEBUG: Jump address = %#X\n", jump_addr);
+        BL_LOG("Jump address = 0x%08lX\n", jump_addr);
         if (bootloader_verify_address(jump_addr) == VALID_ADDR)
         {
             uint8_t valid_addr = VALID_ADDR;
             bootloader_send_data(&valid_addr, 1);
 
-            debug_printf("BOOTLOADER_DEBUG: Valid. Jumping to %#X.\n", jump_addr);
+            BL_LOG("Valid. Jumping to 0x%08lX.\n", jump_addr);
 
             /* Ensure that the last bit in the address is set for it to be a THUMB instruction */
             jump_addr |= 1; 
@@ -230,14 +230,14 @@ void bootloader_cmd_jump_address(uint8_t *buffer)
         }
         else
         {
-            debug_printf("BOOTLOADER_DEBUG: Invalid address!\n");
+            BL_LOG("Invalid address!\n");
             uint8_t invalid_addr = INVALID_ADDR;
             bootloader_send_data(&invalid_addr, 1);
         }
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -247,11 +247,11 @@ void bootloader_cmd_flash_erase(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_flash_erase.\n");
+    BL_LOG("Called bootloader_cmd_flash_erase.\n");
 
     if (bootloader_verify_crc(buffer, packet_length - 4, host_crc) == CRC_STATUS_SUCCESS)
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
         bootloader_send_ack(1);
 
         uint8_t status = bootloader_flash_erase(buffer[2], buffer[3]);
@@ -259,7 +259,7 @@ void bootloader_cmd_flash_erase(uint8_t *buffer)
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -269,11 +269,11 @@ void bootloader_cmd_mem_write(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_mem_write.\n");
+    BL_LOG("Called bootloader_cmd_mem_write.\n");
 
     if (bootloader_verify_crc(buffer, packet_length - 4, host_crc) == CRC_STATUS_SUCCESS)
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
 
         uint32_t base_address = *(uint32_t *)(buffer + 2);
         uint8_t payload_size = buffer[6];
@@ -288,7 +288,7 @@ void bootloader_cmd_mem_write(uint8_t *buffer)
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -298,18 +298,18 @@ void bootloader_cmd_mem_read(uint8_t *buffer)
     uint32_t packet_length = buffer[0] + 1;
     uint32_t host_crc = *(uint32_t *)(buffer + packet_length - 4);
 
-    debug_printf("BOOTLOADER_DEBUG: Called bootloader_cmd_mem_read.\n");
+    BL_LOG("Called bootloader_cmd_mem_read.\n");
 
     if (bootloader_verify_crc(buffer, packet_length - 4, host_crc) == CRC_STATUS_SUCCESS)
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum approved!\n");
+        BL_LOG("CRC checksum approved!\n");
 
         uint32_t base_address = *(uint32_t *)(buffer + 2);
         uint32_t length = buffer[6];
 
         bootloader_send_ack(length + 1);
 
-        debug_printf("BOOTLOADER_DEBUG: Address: %#x, Length: %d.\n", base_address, length);
+        BL_LOG("Address: 0x%08lX, Length: %lu.\n", base_address, length);
 
         uint8_t *response_buffer = (uint8_t *)malloc(length + 1);
 
@@ -317,12 +317,12 @@ void bootloader_cmd_mem_read(uint8_t *buffer)
         uint8_t status = flash_read(base_address, &response_buffer[1], length);
         response_buffer[0] = status;
 
-        debug_printf("BOOTLOADER_DEBUG: Flash read status: %s.\n", status == FLASH_SUCCESS ? "success" : "fail");
+        BL_LOG("Flash read status: %s.\n", status == FLASH_SUCCESS ? "success" : "fail");
         bootloader_send_data(response_buffer, length + 1);
     }
     else
     {
-        debug_printf("BOOTLOADER_DEBUG: CRC checksum failed!\n");
+        BL_LOG("CRC checksum failed!\n");
         bootloader_send_nack();
     }
 }
@@ -372,7 +372,7 @@ uint8_t bootloader_flash_erase(uint8_t base_sector_number, uint8_t num_of_sector
     if (base_sector_number == 0xFF)
     {
         /* Perform mass erase */
-        debug_printf("BOOTLOADER_DEBUG: Performing mass erase of flash memory.\n");
+        BL_LOG("Performing mass erase of flash memory.\n");
         flash_init();
         flash_mass_erase();
         return ERASE_SUCCESS;
@@ -383,16 +383,16 @@ uint8_t bootloader_flash_erase(uint8_t base_sector_number, uint8_t num_of_sector
         base_sector_number > FLASH_SECTOR_7_NUMBER ||
         num_of_sectors > FLASH_SECTOR_7_NUMBER - base_sector_number)
     {
-        debug_printf("BOOTLOADER_DEBUG: Incorrect parameters for flash erase.\n");
+        BL_LOG("Incorrect parameters for flash erase.\n");
         return ERASE_FAILURE;
     }
 
-    debug_printf("BOOTLOADER_DEBUG: Erasing %d sectors starting from %d.\n", num_of_sectors, base_sector_number);
+    BL_LOG("Erasing %d sectors starting from %d.\n", num_of_sectors, base_sector_number);
     flash_init();
     for (uint8_t i = base_sector_number; i < base_sector_number + num_of_sectors; i++)
     {
         flash_sector_erase(i);
-        debug_printf("BOOTLOADER_DEBUG: Erased %d sector.\n", i);
+        BL_LOG("Erased %d sector.\n", i);
     }
 
     return ERASE_SUCCESS;
