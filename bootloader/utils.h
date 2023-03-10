8b7d49b8a0c3c139056f879b0d307d1f52b545c3
Viktor Ciolkowski <wikcioo@gmail.com>
2023-03-10 20:19:25 +0100

    Changed bootloader logging to use a macro.
    If BL_ENABLE_DEBUG_PRINT macro is defined, only then will BL_LOG print log
    information through the uart.

--- bootloader/utils.h
+++ bootloader/utils.h
@@ -6,22 +6,22 @@
 #include <string.h>
 
 #include "stm32f446xx_usart.h"
+
 #define DEBUG_BUFFER_SIZE 256
 
-void debug_printf(char *format, ...)
-{
 #ifdef BL_ENABLE_DEBUG_PRINT
     #ifndef DEBUG_UART
         #error "DEBUG_UART not defined!"
-    #endif
-    char buffer[DEBUG_BUFFER_SIZE];
+    #else
+        #define BL_LOG(format, ...) \
+            do { \
+                char temp_bl_buf[DEBUG_BUFFER_SIZE] = {0}; \
+                snprintf(temp_bl_buf, DEBUG_BUFFER_SIZE, "[%s:%d] " format, __FILE__, __LINE__, ##__VA_ARGS__); \
+                usart_transmit(&DEBUG_UART, (uint8_t *)temp_bl_buf, strlen(temp_bl_buf)); \
+            } while(0)
+    #endif // DEBUG_UART
+#else
+    #define BL_LOG(format, ...)
+#endif // BL_ENABLE_DEBUG_PRINT
 
-    va_list args;
-    va_start(args, format);
-    vsprintf(buffer, format, args);
-    usart_transmit(&DEBUG_UART, (uint8_t *)buffer, strlen(buffer));
-    va_end(args);
-#endif
-}
-
-#endif
+#endif //__UTILS_H__
